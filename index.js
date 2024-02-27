a616105a66913bde2d761253f2e1648543192034
Monica Palko <m.a.palko07@gmail.com>
2024-02-26 21:43:43 -0500

    -a

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
-const { fetchCoordsByIP } = require('./iss');
+const { fetchISSFlyOverTimes } = require('./iss');
 
-fetchCoordsByIP('99.252.134.188', (error, coordinates) => {
+const exampleCoords = { latitude: 43.653226, longitude: -79.3831843 };
+
+fetchISSFlyOverTimes(exampleCoords, (error, passTimes) => {
   if (error) {
     console.log("It didn't work!" , error);
     return;
   }
 
-  console.log('It worked! Returned coordinates:' , coordinates);
+  console.log('It worked! Returned flyover times:' , passTimes);
 });
