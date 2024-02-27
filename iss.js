41ecb42f22a3694cbc2e2975fc427abec579c535
Monica Palko <m.a.palko07@gmail.com>
2024-02-26 21:39:39 -0500

    -a

--- iss.js
+++ iss.js
@@ -1,26 +1,34 @@
 const request = require('request');
-
 /**
- * Makes a single API request to retrieve the user's IP address.
+ * Makes a single API request to retrieve the lat/lng for a given IPv4 address.
  * Input:
- *   - A callback (to pass back an error or the IP string)
+ *   - The ip (ipv4) address (string)
+ *   - A callback (to pass back an error or the lat/lng object)
  * Returns (via Callback):
  *   - An error, if any (nullable)
- *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ *   - The lat and lng as an object (null if error). Example:
+ *     { latitude: '49.27670', longitude: '-123.13000' }
  */
+const fetchCoordsByIP = function(ip, callback) {
+  request(`http://ipwho.is/${ip}`, (error, response, body) => {
 
-const fetchMyIP = function(callback) { 
-  request('https://api.ipify.org?format=json', (error, response, body) => {
-    if (error) return callback(error, null);
-
-    if (response.statusCode !== 200) {
-      callback(Error(`Status Code ${response.statusCode} when fetching IP. Response: ${body}`), null);
+    if (error) {
+      callback(error, null);
       return;
     }
 
-    const ip = JSON.parse(body).ip;
-    callback(null, ip);
+    const parsedBody = JSON.parse(body);
+
+    if (!parsedBody.success) {
+      const message = `Success status was ${parsedBody.success}. Server message says: ${parsedBody.message} when fetching for IP ${parsedBody.ip}`;
+      callback(Error(message), null);
+      return;
+    } 
+
+    const { latitude, longitude } = parsedBody;
+
+    callback(null, {latitude, longitude});
   });
 };
 
-module.exports = { fetchMyIP };
+module.exports = { fetchCoordsByIP };
